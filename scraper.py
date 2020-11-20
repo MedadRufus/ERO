4cf0427babdda86bf9b2eea49023ba3d91b5492d
MedadRufus <mailto@medadnewman.co.uk>
2020-11-20 15:13:29 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -7,19 +7,15 @@ from bs4 import BeautifulSoup
 if __name__ == "__main__":
 
     conn = requests.get("https://www.tripadvisor.co.uk/Attractions-g294265-Activities-c26-Singapore.html#FILTERED_LIST")
-
-
     soup = BeautifulSoup(conn.text, 'html.parser')
-
-
     data = soup.find('script', type='application/ld+json').contents
-    #print(data)
-    pp = pprint.PrettyPrinter(indent=4)
+
+
     result = trip_advisor_page_from_dict(json.loads(data[0]))
     result_dict = trip_advisor_page_to_dict(result)
 
     for i in result.item_list_element:
         print(i.name,":",i.url)
 
-
+    #pp = pprint.PrettyPrinter(indent=4)
     #pp.pprint(result_dict)
